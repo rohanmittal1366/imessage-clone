38b230d5d4941518eac1a534c28d78a6b9a763db
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-17 22:34:14 +0530

    initial commit

---
+++ src/message/Imessage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./imessage.css";
+function Imessage() {
+  return (
+    <div className="imessage">
+      {/* Sidebar  */}
+      <Sidebar />
+      {/* chat */}
+    </div>
+  );
+}
+
+export default Imessage;
