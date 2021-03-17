21468616b70b9296af6ea3e4c4a6cc78b20dfe5e
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-18 02:06:39 +0530

    chat section is added

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React , {useState} from "react";
 import "./chat.css";
 
 function Chat() {
-    return (
-        <div className="chat">
-            
-        </div>
-    );
+  const [input, setInput] = useState("");
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    setInput("");
+  };
+
+  return (
+    <div className="chat">
+      {/* chatHeader */}
+      <div className="chat__header">
+        <h4>
+          To: <span className="chat__name">channel Name</span>
+        </h4>
+        <strong>Details</strong>
+      </div>
+      {/* chat messages */}
+
+      {/* chat inputs */}
+      <div className="chat__input">
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="imessage"
+            type="text"
+          />
+          <button onClick={sendMessage}>Send Messages</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
