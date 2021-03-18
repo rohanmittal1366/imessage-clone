a3f7c4a6fed9b9445cbebf11c6622412cb3272e1
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-18 19:37:08 +0530

    sidebar chat components are set dynamically

--- src/Components/Messages/Message.js
+++ src/Components/Messages/Message.js
@@ -1,16 +1,19 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import { selectUser } from "../../features/userSlice";
 import "./message.css";
+import { useSelector } from "react-redux";
 
-function Message({
+const Message = ({
   id,
   contents: { timestamp, displayName, email, message, photo, uid },
-}) {
+}) => {
+  const user = useSelector(selectUser);
   return (
-    <div className="message">
+    <div className={`message ${user.email == email && "message__sender"}`}>
       <Avatar src={photo} />
       <p>{message}</p>
-      <small>{new Date(timestamp?.toDate()).toUTCString()}</small>
+      <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
     </div>
   );
 }
--- src/Components/Sidebar/SidebarChat/SidebarChat.js
+++ src/Components/Sidebar/SidebarChat/SidebarChat.js
@@ -1,11 +1,24 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setChat } from "../../../features/chatSlice";
+import db from "../../../firebase/firebase";
 import "./sidebarChat.css";
+
 
 function SidebarChat({ id, chatName }) {
   const dispatch = useDispatch();
+  const [chatInfo, setChatInfo] = useState([]);
+
+  useEffect(() => {
+    db.collection("chats")
+      .doc(id)
+      .collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setChatInfo(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, [id]);
 
   return (
     <div
@@ -19,11 +32,12 @@ function SidebarChat({ id, chatName }) {
         )
       }
     >
-      <Avatar />
+      <Avatar src ={chatInfo[0]?.photo} />
       <div className="sidebarChat__info">
         <h3>{chatName}</h3>
-        <p>Last message sent...</p>
-        <small>Timestamp</small>
+        <p>{chatInfo[0]?.message}</p>
+        <small>{new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString()}</small>
+    
       </div>
     </div>
   );
