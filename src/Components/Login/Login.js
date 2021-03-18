4191b67437c931cbff8c70ec65059056c5b165eb
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-18 14:27:18 +0530

    firebase bug is resolved

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./login.css";
+import { auth, provider } from "../../firebase/firebase";
 
 function Login() {
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
-      
       <div className="login__logo">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlzhKvw4sZcK3t2ihfVuerdAu6mwBEHPS3rA&usqp=CAU"
@@ -12,7 +16,7 @@ function Login() {
         />
         <h1>iMessage</h1>
       </div>
-      <button>Sign In</button>
+      <button onClick={signIn}>Sign In</button>
     </div>
   );
 }
