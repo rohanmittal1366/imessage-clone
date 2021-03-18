504d6d9067d6c040d26db7397968dc156223f1b3
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-18 09:53:07 +0530

    firebase file is added

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'; 
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBEOobOJH735PxAG7Eh_w2l6vtsuhA0VqY",
+    authDomain: "imessage-clone-fcd46.firebaseapp.com",
+    projectId: "imessage-clone-fcd46",
+    storageBucket: "imessage-clone-fcd46.appspot.com",
+    messagingSenderId: "597552039234",
+    appId: "1:597552039234:web:36773dfd59aff02b7774db",
+    measurementId: "G-PJ14YQNJL7"
+  };
